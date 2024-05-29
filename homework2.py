def6e5bad896d43fd4cd6caf655de4cd4465a9c4
Erdych <jeffmoreaujo@gmail.com>
2024-05-29 12:01:46 +0700

    Add files via upload
    Практическое задание по теме: "Переменные"

---
+++ homework2.py
@@ -0,0 +1,6 @@
+homework_done = 12
+hours_spent = 1.5
+course_name = 'Python'
+time_per_task = hours_spent / homework_done # Расчёт временных затрат на задачу
+# Один из вариантов решений
+print('Курс: ' + course_name + ',', 'всего задач: ' + str(homework_done) + ',', 'затрачено часов: ' + str(hours_spent) + ',', 'среднее время выполнения ' + str(time_per_task) + ' часа.')
