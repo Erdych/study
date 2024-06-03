bd27ee4b9ece425471420d2f2fecb12492089923
Erdych <jeffmoreaujo@gmail.com>
2024-06-03 16:10:25 +0700

    Update module1hard.py
    Применён цикл for для передачи парных значений словарю

--- module1hard.py
+++ module1hard.py
@@ -15,21 +15,10 @@ students = {
     'Khendrik',
     'Aaron'
 }
-students_sorted = list(students)
-# Создана переменная, которая хранит список с именами студентов, необходимая для следующей операции
-students_sorted.sort() 
-# Теперь список сортирован в алфавитном порядке
-# Каждый элемент принял порядковый номер, совпадающий с индексом соответствующего ему элемента из списка оценок (grades)
-
-average_grades.update(
-        {
-            students_sorted[0] : sum(grades[0]) / len(grades[0]), 
-            students_sorted[1] : sum(grades[1]) / len(grades[1]),
-            students_sorted[2] : sum(grades[2]) / len(grades[2]),
-            students_sorted[3] : sum(grades[3]) / len(grades[3]),
-            students_sorted[4] : sum(grades[4]) / len(grades[4])
-        }  
-    )
-# sum(grades[x]) / len(grades[x]) # Формула для нахождения среднего балла
-
+student_list = sorted(students)
+for i in range(0, len(student_list)):
+    average_grades.update({student_list[0] : sum(grades[0]) / len(grades[0])})
+    student_list.remove(student_list[0])
+    grades.remove(grades[0])
+    
 print(average_grades)
