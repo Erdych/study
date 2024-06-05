9f37e8388007381ca4426900ca11e3e0269b3072
Erdych <jeffmoreaujo@gmail.com>
2024-06-05 20:51:30 +0700

    Update module_2_3.py
    Добавлены другие варианты решения.

--- module_2_3.py
+++ module_2_3.py
@@ -1,3 +1,4 @@
+# Решение 1.
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 
 while my_list != []:
@@ -6,4 +7,29 @@ while my_list != []:
     else:
         my_list.pop(0)
         
+print('Code executed')
+
+# Решение 2.
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+
+while True:
+    if my_list == []:
+        break
+    elif my_list[0] > 0:
+        print(my_list.pop(0))
+        continue
+    else:
+        my_list.pop(0)
+        continue
+
+print('Code executed')
+
+# Решение 3. С использованием цикла for
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+for el in my_list:
+    if el > 0:
+        print(el)
+    else:
+        continue
+
 print('Code executed')
